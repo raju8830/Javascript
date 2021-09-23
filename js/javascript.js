e72ff88b4935eaca41693051d2f39ae0b80c4d83
raju <rraju8830@gmail.com>
2021-09-23 23:43:26 +0600

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+console.log("Hello World")
+
+// Script With Font Style
+
+let font = 360;
+        document.getElementById('demo').style.fontSize = `${font}px`;
+console.log(font)
+if (font < 345){
+    document.getElementById('demo').style.fontSize = "10px";
+}else if (font > 344){
+    document.getElementById('demo').style.fontSize = "30px"
+}
+
+// Script Display Proparty
+
+// document.getElementById('block').style.display = "none";
+// console.log(block)
+
+
+// Home Alart
+//window.alert('Hello Bangladesh')
+
+// Home Page Ptints
+
+// function print(){
+//     document.getElementById('myPrint').window.print();
+// }
+
